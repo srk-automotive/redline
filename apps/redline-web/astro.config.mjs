61207ef8857f05df554966117da60c5af0421505
Rishi Khiroya <rishi@vrish.dev>
2026-03-30 16:57:44 +0100

    chore: Initial commit
    Initial commit from create-turbo
    
    Remove apps/docs
    
    Rename packages/ui to packages/ui-react
    
    Add new library packages/ui-solid
    
    Init apps/redline-web as an astro project

---
+++ apps/redline-web/astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+import solidJs from '@astrojs/solid-js';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), solidJs()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
