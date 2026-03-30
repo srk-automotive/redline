ae464ef2ef74864ba13c437b3884399f2cfb971b
Rishi Khiroya <rishi@vrish.dev>
2026-03-30 18:01:42 +0100

    build: Update astro config to isolate react and solidjs components

--- apps/redline-web/astro.config.mjs
+++ apps/redline-web/astro.config.mjs
@@ -1,15 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import solidJs from '@astrojs/solid-js';
-import tailwindcss from '@tailwindcss/vite';
+import react from "@astrojs/react";
+import solidJs from "@astrojs/solid-js";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), solidJs()],
+  integrations: [
+    react({
+      include: ["**/ui-react/**"],
+    }),
+    solidJs({
+      include: ["**/ui-solid/**"],
+    }),
+  ],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
